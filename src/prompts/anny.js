f2dec736c1798ca3a2d580e58f4fbd53240215c4
tioopental <tioopental@gmail.com>
2023-05-28 23:31:51 +0200

    Update anny.js
    added prompts

--- src/prompts/anny.js
+++ src/prompts/anny.js
@@ -19,6 +19,16 @@ export default {
     '"fkin"',
     'gets OneGuy\'ed',
     '"we are not dating!"',
+    'gets hit on by Neuro',
+    'asks Vedal for help',
+    'dies from laughter',
+    'smooth brain moment',
+    '"Neuro is so cute"',
+    'leaks something',
+    'roast Vedal',
+    'educate\'s Neuro',
+    'exposed by Neuro',
+    'obvious lie',
 
     // mentions
     'mentions she is Neuro\'s mom'
@@ -27,6 +37,7 @@ export default {
     'chat spams AnnyLebronJam',
     'chat spams LUBBERS',
     'chat spams BOOBA',
+    'chat dies from cringe',
     
     //twitch meta
     'morari drops 50 or more subs',
