621dd0c734d3b320b226e5ebee2afeebc09aa889
CJMAXiK <me@cjmaxik.ru>
2023-05-28 17:51:08 +0300

    Initial collab support

---
+++ src/prompts/filian.js
@@ -0,0 +1,50 @@
+export default {
+  id: 'filian',
+  name: 'filian',
+  color: '#be01fe',
+
+  image: 'fruitSnacks.png',
+
+  sounds: [
+    'fruitSnacks.mp3'
+  ],
+
+  prompts: [
+    'funny joke',
+    'does a flip',
+    'flash bang',
+    'begs Vedal for help',
+    'roasts Neuro',
+    'deez nuts joke',
+    'drops headset',
+    'gaslights Neuro',
+    '"STOP!"',
+    'effin dies',
+    'makes strange noise',
+    'W RIZZ',
+    'frick',
+    'barks',
+    'rick rolled',
+    'falls from chair',
+    'camera goes crazy',
+    'falls from ledge',
+    'VR gear bugs out',
+    'breaks characater',
+    'hits the wall',
+
+    // model related
+    'bunny girl model',
+    'police filian model',
+    'kawaii filian model',
+
+    // mentions
+    'mentions projektmelody',
+    'mentions Fruit Snacks',
+    'mentions Rekson',
+
+    // chat related
+    'chat L\'s filian',
+    'chat W\'s filian',
+    'chat dies of cringe'
+  ]
+}
