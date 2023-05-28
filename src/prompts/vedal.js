ece9d54baaa7578953fe16134a60d886f9357084
tioopental <tioopental@gmail.com>
2023-05-28 19:02:18 +0200

    Create vedal.js
    - no image
    - no sounds
    - added prompts
    - added prompts mentions
    - added prompts chat related

---
+++ src/prompts/vedal.js
@@ -0,0 +1,32 @@
+export default {
+  id: 'vedal',
+  name: 'vedal',
+  color: 'tba',
+
+  image: 'tba.png',
+
+  sounds: [
+    'tba.mp3'
+  ],
+
+  prompts: [
+    'cringe joke',
+    '"Shut up"',
+    'aruges with Alex',
+    'drunk stream',
+    'argues with Neuro',
+    '"That\'s crazy 🐢"',
+    'code should work, but doesn\'t',
+    'gets hit on by Neuro',
+    'leaks something',
+    
+    // mentions
+    'mentions anny',
+    'mentions programming socks',
+
+    // chat related
+    'chat shoots Tutel',
+    'chat demands attention to Neuro',
+    'chat spams KEKW'
+  ]
+}
