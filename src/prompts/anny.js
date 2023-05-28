366e232e8c2fc7fe5fd58b76f6f5e29f60a231b1
tioopental <tioopental@gmail.com>
2023-05-28 19:05:23 +0200

    Create anny.js
    - no image
    - no sounds
    - added prompts
    - no mentions
    - added propmts chat related

---
+++ src/prompts/anny.js
@@ -0,0 +1,30 @@
+export default {
+  id: 'anny',
+  name: 'anny',
+  color: 'tba',
+
+  image: 'tba.png',
+
+  sounds: [
+    'tba.mp3'
+  ],
+
+  prompts: [
+    'lewd joke',
+    '"HIC"',
+    'makes sussy noise',
+    'RIP desk',
+    'blames Vedal',
+    'YABAI statement',
+    '"fkin"',
+    'gets OneGuy\'ed',
+    '"we are not dating!"',
+
+    // mentions
+
+    // chat related
+    'chat spams AnnyLebronJam',
+    'chat spams LUBBERS',
+    'chat spams BOOBA',
+  ]
+}
