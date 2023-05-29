d800d879c3ed9823221232d56b3c75bc527c14cb
CJMAXiK <me@cjmaxik.ru>
2023-05-29 09:45:26 +0300

    Update anny prompts and sound bit

---
+++ src/components/StreamTypeItem.vue
@@ -0,0 +1,30 @@
+<template>
+  <q-item
+    clickable
+    dense
+    tag="a"
+    :href="href"
+  >
+    <q-item-section>
+      <q-item-label>{{ text }}</q-item-label>
+    </q-item-section>
+  </q-item>
+</template>
+
+<script setup>
+import { computed } from 'vue'
+
+const props = defineProps({
+  streamType: {
+    type: String,
+    required: true
+  },
+  text: {
+    type: String,
+    required: true
+  }
+})
+
+const href = computed(() => props.streamType === '/' ? '/' : `/${props.streamType ?? ''}`)
+console.log(href)
+</script>
--- src/prompts/prompts.js
+++ src/prompts/prompts.js
@@ -1,5 +1,6 @@
-import filian from './filian'
 import neuro from './neuro'
+import filian from './filian'
+import anny from './anny'
 
 /**
  * 1. The array name is the route path
@@ -17,6 +18,13 @@ export default {
     name: 'Neuro x filian collab',
     participants: [
       neuro, filian
+    ]
+  },
+
+  annyCollab: {
+    name: 'Neuro x anny collab',
+    participants: [
+      neuro, anny
     ]
   }
 }
