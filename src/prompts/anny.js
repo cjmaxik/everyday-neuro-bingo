877672ce78f7016804e73c93c471e5f7582305dd
tioopental <tioopental@gmail.com>
2023-05-29 08:20:28 +0200

    Create anny prompts and assets (#14)
    * Create vedal.js
    
    - no image
    - no sounds
    - added prompts
    - added prompts mentions
    - added prompts chat related
    
    * Create anny.js
    
    - no image
    - no sounds
    - added prompts
    - no mentions
    - added propmts chat related
    
    * Update neuro.js
    
    more prompts
    
    * Update anny.js
    
    fixed
    - color
    - image
    - sound
    
    * Create annystar.png
    
    * Add files via upload
    
    * Create annyhic.mp3
    
    * Add files via upload
    
    * Update anny.js
    
    added prompts
    
    * Fix neuro.js omg
    
    * Update src/prompts/vedal.js
    
    Co-authored-by: CJMAXiK <dj.cjmaxik@gmail.com>
    
    * Update src/prompts/vedal.js
    
    Co-authored-by: CJMAXiK <dj.cjmaxik@gmail.com>
    
    * Delete vedal.js
    
    ---------
    
    Co-authored-by: CJMAXiK <me@cjmaxik.ru>
    Co-authored-by: CJMAXiK <dj.cjmaxik@gmail.com>

---
+++ src/prompts/anny.js
@@ -0,0 +1,45 @@
+export default {
+  id: 'anny',
+  name: 'anny',
+  color: '#feb3c8',
+
+  image: 'annystar.png',
+
+  sounds: [
+    'annyhic.mp3'
+  ],
+
+  prompts: [
+    'lewd joke',
+    '"HIC"',
+    'makes sussy noise',
+    'RIP desk',
+    'blames Vedal',
+    'YABAI statement',
+    '"fkin"',
+    'gets OneGuy\'ed',
+    '"we are not dating!"',
+    'gets hit on by Neuro',
+    'asks Vedal for help',
+    'dies from laughter',
+    'smooth brain moment',
+    '"Neuro is so cute"',
+    'leaks something',
+    'roast Vedal',
+    'educate\'s Neuro',
+    'exposed by Neuro',
+    'obvious lie',
+
+    // mentions
+    'mentions she is Neuro\'s mom'
+
+    // chat related
+    'chat spams AnnyLebronJam',
+    'chat spams LUBBERS',
+    'chat spams BOOBA',
+    'chat dies from cringe',
+    
+    //twitch meta
+    'morari drops 50 or more subs',
+  ]
+}
