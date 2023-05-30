35f9e9bf078af9cc12f00a3777f042445b180b10
tioopental <tioopental@gmail.com>
2023-05-30 18:23:52 +0200

    Create neuroshylily.js
    - added Shyliliy related
    - balanced rest of prompts

---
+++ src/prompts/neuroshylily.js
@@ -0,0 +1,51 @@
+export default {
+  id: 'neuroshylily',
+  name: 'Neuro-sama',
+
+  // text color
+  color: '#8b3f5e',
+  
+  // tally background image
+  image: 'gymbag.png',
+
+  // tally sounds, will be chosen at random
+  sounds: [
+    'KEKWA.mp3',
+    'gymbag.mp3',
+    'no.mp3',
+    'meow_meow_lol.mp3',
+    'heart.mp3'
+  ],
+
+  prompts: [
+    'makes sussy noise',
+    'funny joke',
+    'tries to leave',
+    '1984',
+    'BASED-sama',
+    'goes schizo',
+    '#ad / corpa',
+    'rants',
+    'apologises profusely',
+    
+    // Shylily related
+    'misgenders Lily',
+    'shames Lily',
+    'calls Lily an AI',
+    'invents new nickname for Lily',
+    'rizzes Lily',
+    'gaslights Lily',
+
+    // mentions
+    'mentions Vedal (randomly)',
+    'mentions random VTuber',
+    'mentions Swarm',
+
+    // chat related
+    'chat spams Clueless',
+    'chat spams PauseSama',
+    'chat spams aware',
+    'chat L\'s Neuro',
+    'chat W\'s Neuro'
+  ]
+}
