541283f8d216c9fb1da79519089cc8c56e756d5b
tioopental <tioopental@gmail.com>
2023-06-01 21:57:23 +0200

    Create neuroEvil.js
    balancing prompts between neuroEvil.js and evilsama.js

---
+++ src/prompts/neuroEvil.js
@@ -0,0 +1,42 @@
+import neuro from './neuro'
+
+export default {
+  id: 'neuroEvil',
+  name: neuro.name,
+  assetsFolder: neuro.id,
+  color: neuro.color,
+  image: neuro.image,
+  sounds: neuro.sounds,
+
+  prompts: [
+    'FUN AI',
+    'problem with her AI',
+    'UwU',
+    'KEKW',
+    'BASED-sama',
+    'apologises profusely',
+    '#ad / corpa',
+    'fact of the day',
+    'meow',
+
+    // Evil related
+    'misgenders Evil',
+    'shames Evil',
+    'calls Evil an AI',
+    'invents new nickname for Evil',
+
+    // mentions
+    'mentions random VTuber',
+    'mentions Bocchi The Rock',
+    'mentions anny',
+    'mentions Abandoned Archive',
+
+    // chat related
+    'chat spams Clueless',
+    'chat spams PauseSama',
+    'chat spams aware',
+    'chat L\'s Neuro',
+    'chat W\'s Neuro',
+    'chat spams Classic'
+  ]
+}
