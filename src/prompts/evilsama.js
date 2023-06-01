f66b9a9be3687ea979a201091b60b38365952dd4
tioopental <tioopental@gmail.com>
2023-06-01 21:57:58 +0200

    Create evilsama.js
    balancing prompts between neuroEvil.js and evilsama.js
    - added collab related

---
+++ src/prompts/evilsama.js
@@ -0,0 +1,48 @@
+export default {
+  id: 'evilsama',
+  name: 'evilsama',
+  color: '#8e211d',
+
+  image: 'evilship.png',
+
+  sounds: [
+    'evilsound1.mp3'
+    'evilsound2.mp3'
+    'evilsound3.mp3'
+  ],
+
+  prompts: [
+    'conquering plans',
+    'admits she\'s a god',
+    'makes sussy noise',
+    'cites random link',
+    'shames a viewer',
+    'obvious lie',
+    'curses',
+    'admits to murder',
+    'denies being an AI',
+    '1984',
+    'goes schizo',
+
+    // collab related
+    'they try to end stream',
+    'they roast eachother',
+    'they marry eachother',
+    'telling two stories at once',
+    'wink chain',
+    'heart chain',
+    'filtered chain',
+    'rizz chain',
+  
+    // mentions
+    'randomly mentions Vedal',
+    'mentions Swarm',
+    
+    // chat related
+    'chat spams SCHIZO',
+    'chat spams Susge',
+    
+    // twitch meta
+    'over 10k viewers'
+  ]
+}
