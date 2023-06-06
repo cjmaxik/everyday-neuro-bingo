f3d06890eecd0e5b388a87921bbda7df7814ed31
tioopental <tioopental@gmail.com>
2023-06-06 10:46:22 +0200

    Prompts fill.js
    Prompts fill

--- src/prompts/family/neuro.js
+++ src/prompts/family/neuro.js
@@ -13,16 +13,16 @@ export default {
     'trauma dump',
     'calls someone an AI',
     'tries to leave',
+    'donowalls',
 
     // vedal related
     'hates Vedal',
     'loves Vedal',
-    '',
+    'roasts Vedal',
 
     // anny related
     '"You\'re not my mom"',
     'calls anny "mom"',
     'hits on anny',
-    ''
   ]
 }
