81535f0d0c5f9ad6f38b73c23b1833c85f6057cd
CJMAXiK <me@cjmaxik.ru>
2023-06-07 21:22:39 +0300

    Index menu changes

--- src/components/StreamTypeLinks.vue
+++ src/components/StreamTypeLinks.vue
@@ -1,30 +1,15 @@
 <template>
   <div class="bg-white shadow-5 shadow-transition">
     <q-list
-      bordered
       separator
       class="q-mt-sm text-center text-h6"
     >
       <StreamTypeItem
-        to="justChatting"
-        text="Chill stream"
-      />
-
-      <StreamTypeItem
-        to="devStream"
-        text="Dev stream"
-      />
-
-      <StreamTypeItem
-        to="familyCollab"
-        text="Family stream"
-        is-new
-      />
-
-      <StreamTypeItem
-        to="evilNeuro"
-        text="Evil Neuro solo"
-        is-new
+        v-for="(stream, index) in regularStreams"
+        :key="index"
+        :to="stream.to"
+        :text="stream.text"
+        :featured="stream.isFeatured"
       />
 
       <q-expansion-item
@@ -34,28 +19,19 @@
         toggle
         expand-icon-class="collab-toggle"
       >
-        <StreamTypeItem
-          to="evilCollab"
-          text="Evil Neuro"
-        />
-
-        <q-separator />
-        <StreamTypeItem
-          to="filianCollab"
-          text="filian"
-        />
-        <q-separator />
-
-        <StreamTypeItem
-          to="annyCollab"
-          text="anny"
-        />
-        <q-separator />
-
-        <StreamTypeItem
-          to="shylilyCollab"
-          text="Shylily"
-        />
+        <div class="row">
+          <div
+            v-for="(stream, index) in collabStreams"
+            :key="index"
+            class="col-6 collabs"
+          >
+            <StreamTypeItem
+              :to="stream.to"
+              :text="stream.text"
+              :featured="stream.isFeatured"
+            />
+          </div>
+        </div>
       </q-expansion-item>
     </q-list>
   </div>
@@ -64,4 +40,28 @@
 <script setup>
 // project-related
 import StreamTypeItem from './StreamTypeItem.vue'
+
+const regularStreams = [
+  { to: 'evilNeuro', text: 'Evil Neuro solo', isFeatured: true },
+  { to: 'justChatting', text: 'Chill stream' },
+  { to: 'devStream', text: 'Dev stream' },
+  { to: 'familyCollab', text: 'Family stream' }
+]
+
+const collabStreams = [
+  { to: 'evilCollab', text: 'Evil Neuro' },
+  { to: 'filianCollab', text: 'filian' },
+  { to: 'annyCollab', text: 'anny' },
+  { to: 'shylilyCollab', text: 'Shylily' }
+]
 </script>
+
+<style lang="scss">
+.collabs {
+  border-top: 1px solid rgba(0, 0, 0, 0.12);
+
+  &:nth-child(odd) {
+    border-right: 1px solid rgba(0, 0, 0, 0.12);
+  }
+}
+</style>
