c01274b75059c820529123f6ae49346dcafb55bd
tioopental <tioopental@gmail.com>
2023-06-25 09:20:02 +0200

    Vei collab Vei collab draft
    Note: As of 25.06.23 collab was not announced

---
+++ src/prompts/vei/vei.js
@@ -0,0 +1,44 @@
+export default {
+    id: 'vei',
+    name: 'vei',
+    color: '#52565a',
+  
+    image: 'pepo.png',
+  
+    sounds: [
+      'tba.mp3',
+    ],
+  
+    prompts: [
+      'lewd joke',
+      'sodapoppin shows up',
+      'makes sussy noise',
+      'lewd joke',
+      'dies from laughter',
+      '',
+      '',
+      '',
+      '',
+      '',
+      '',
+      '',
+      '',
+      '',
+      '',
+      '',
+      
+      // Neuro related
+      'gets hit on by Neuro',
+      '',
+      '',
+      '',
+      '',
+  
+      // chat related
+      'chat spams :uuh:',
+      'chat spams :BAND:',
+      'chat spams :BOOBA:',
+      'chat dies from cringe'
+    ]
+  }
+  
---
+++ src/prompts/vei/veiCollab.js
@@ -0,0 +1,10 @@
+import neuro from './neuro'
+import vei from './vei'
+
+export default {
+  name: 'Neuro x Vei collab',
+  image: 'vei/veio7.png',
+  participants: [
+    neuro, vei
+  ]
+}
