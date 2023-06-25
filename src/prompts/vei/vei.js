5788b168eb7ab93f40420c52dfffeebe42431729
CJMAXiK <me@cjmaxik.ru>
2023-06-25 10:41:58 +0300

    Update assets

--- src/prompts/vei/vei.js
+++ src/prompts/vei/vei.js
@@ -1,44 +1,43 @@
 export default {
-    id: 'vei',
-    name: 'vei',
-    color: '#52565a',
-  
-    image: 'pepo.png',
-  
-    sounds: [
-      'tba.mp3',
-    ],
-  
-    prompts: [
-      'lewd joke',
-      'sodapoppin shows up',
-      'makes sussy noise',
-      'lewd joke',
-      'dies from laughter',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      
-      // Neuro related
-      'gets hit on by Neuro',
-      '',
-      '',
-      '',
-      '',
-  
-      // chat related
-      'chat spams :uuh:',
-      'chat spams :BAND:',
-      'chat spams :BOOBA:',
-      'chat dies from cringe'
-    ]
-  }
-  
+  id: 'vei',
+  name: 'vei',
+  color: '#52565a',
+
+  image: 'pepo.png',
+
+  sounds: [
+    ''
+  ],
+
+  prompts: [
+    'lewd joke',
+    'sodapoppin shows up',
+    'makes sussy noise',
+    'lewd joke',
+    'dies from laughter',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+
+    // Neuro related
+    'gets hit on by Neuro',
+    '',
+    '',
+    '',
+    '',
+
+    // chat related
+    'chat spams :uuh:',
+    'chat spams :BAND:',
+    'chat spams :BOOBA:',
+    'chat dies from cringe'
+  ]
+}
