2446797642631baf4c1d5d3eb98bb17f016f378c
CJMAXiK <me@cjmaxik.ru>
2023-07-03 12:26:27 +0300

    Dark theme support

--- quasar.config.js
+++ quasar.config.js
@@ -115,7 +115,8 @@ module.exports = configure(function (/* ctx */) {
       plugins: [
         'Notify',
         'AppFullscreen',
-        'AppVisibility'
+        'AppVisibility',
+        'Dark'
       ]
     },
 
