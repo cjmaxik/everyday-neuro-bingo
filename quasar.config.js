244cf9963ef67a2cf300f80b32e12ca5710b10d7
CJMAXiK <me@cjmaxik.ru>
2023-07-03 14:47:16 +0300

    Enable auto mode

--- quasar.config.js
+++ quasar.config.js
@@ -99,6 +99,7 @@ module.exports = configure(function (/* ctx */) {
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#framework
     framework: {
       config: {
+        dark: 'auto',
         screen: {
           bodyClasses: true
         }
