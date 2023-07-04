f4338e951bb5116fb9629389b8c8dcab81f4f6d7
CJMAXiK <me@cjmaxik.ru>
2023-07-04 13:43:54 +0300

    Generic character

---
+++ src/prompts/characters/generic.js
@@ -0,0 +1,12 @@
+// Use only when you don't have identity for the character
+
+export default {
+  assetsFolder: 'generic',
+
+  image: 'generic.png',
+
+  // tally sounds, will be chosen at random
+  sounds: [
+    'generic.mp3'
+  ]
+}
