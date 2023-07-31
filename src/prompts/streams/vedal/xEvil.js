589e0e58726d267dcf3f43d320cb6d4cd1b11f59
CJMAXiK <me@cjmaxik.ru>
2023-07-31 14:42:10 +0300

    Evil x Vedal prompts

---
+++ src/prompts/streams/vedal/xEvil.js
@@ -0,0 +1,51 @@
+import { mergeUnique } from 'helpers/helpers'
+
+import * as evil from 'characters/evilNeuro'
+import * as vedal from 'characters/vedal'
+import * as chat from 'characters/chat'
+import * as genericCollab from 'characters/genericCollab'
+
+const collabPrompts = evil.collabPrompts('Vedal')
+
+const evilRegular = {
+  ...evil.base,
+
+  prompts: evil.regularPrompts
+    .filter(x => !collabPrompts.includes(x)) // removes duplicated Vedal prompts
+}
+
+const evilCollab = {
+  ...evil.base,
+
+  prompts: collabPrompts
+    .map(x => x.replace('calls Vedal Vedal', 'mispronounses Vedal\'s name'))
+}
+
+const vedalRegular = {
+  ...vedal.base,
+
+  prompts: mergeUnique(
+    vedal.regularPrompts,
+    vedal.mentionPrompts,
+    vedal.collabPrompts,
+    genericCollab.collabPrompts
+  )
+}
+
+const chatPrompts = {
+  ...chat.base,
+
+  prompts: mergeUnique(
+    evil.chatPrompts,
+    vedal.chatPrompts,
+    chat.chatPrompts
+  )
+}
+
+export default {
+  name: 'Evil and Vedal',
+  image: 'vedal/vedalCorpa.png',
+  participants: [
+    evilRegular, vedalRegular, chatPrompts, evilCollab
+  ]
+}
