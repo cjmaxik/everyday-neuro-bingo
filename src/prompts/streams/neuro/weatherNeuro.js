3eea435a1f29d2424f3384410616498ae1e94b42
CJMAXiK <me@cjmaxik.ru>
2023-08-10 06:15:15 +0300

    Initial prompts

--- src/prompts/streams/neuro/weatherNeuro.js
+++ src/prompts/streams/neuro/weatherNeuro.js
@@ -1,6 +1,29 @@
 import * as neuro from 'characters/neuro'
 import * as chat from 'characters/chat'
 
+const weatherNeuro = {
+  ...neuro.base,
+
+  id: 'weatherNeuro',
+  color: '#0000ff',
+  name: 'Weather reports',
+
+  prompts: [
+    'too cold where it shouldn\'t be',
+    'too warm where it shouldn\'t be',
+    'apocalypse forecast',
+    '69°',
+    'degree Celsius',
+    'degree Fahrenheit',
+    'degree Kelvin',
+    'weather is unknown',
+    'reasonable forecast',
+    'weather in Vedaltopia',
+    'weather in fictional place',
+    'raining cats\xa0/\xa0dogs'
+  ]
+}
+
 const regularNeuro = {
   ...neuro.base,
 
@@ -19,16 +42,6 @@ const specificNeuro = {
   prompts: neuro.specificPrompts
 }
 
-const mentions = {
-  ...neuro.base,
-
-  id: 'mentions',
-  color: '#420069',
-  name: 'Mentions',
-
-  prompts: neuro.mentionPrompts
-}
-
 const chatPrompts = {
   ...chat.base,
 
@@ -39,9 +52,9 @@ const chatPrompts = {
 }
 
 export default {
-  name: 'Neuro-sama solo',
+  name: 'Weather Report Stream',
   image: 'neuro/random/neuro{x}.png',
   participants: [
-    regularNeuro, mentions, specificNeuro, chatPrompts
+    regularNeuro, weatherNeuro, specificNeuro, chatPrompts
   ]
 }
