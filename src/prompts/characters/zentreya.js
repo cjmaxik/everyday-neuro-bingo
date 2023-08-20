e3d71bd941b8d0a03d21a8b0bf16d72d3c2492f7
CJMAXiK <me@cjmaxik.ru>
2023-08-20 20:23:46 +0300

    Working board

--- src/prompts/characters/zentreya.js
+++ src/prompts/characters/zentreya.js
@@ -1,28 +1,44 @@
-export default {
+export const base = {
   id: 'zentreya',
   name: 'Zentreya',
   color: '#d4233f',
 
   image: '',
 
-  sounds: [],
+  sounds: []
+}
+
+export const regularPrompts = [
+  // generic prompts
+  'dies from laughter',
+  'funny joke',
+  'cringe joke',
+  'sussy noise',
+  'meow~',
+
+  // Neuro-related
+  'praises Neuro',
+  'argues with Neuro',
+  'gaslights Neuro',
+  'loves Neuro',
+  'hates Neuro',
+  'roasts Neuro',
+  'shames Neuro',
+  'W RIZZ'
+]
+
+export const chatPrompts = [
+  // chat related
+  'chat L\'s Zen',
+  'chat W\'s Zen',
+  'chat dies of cringe'
+]
+
+export default {
+  ...base,
 
   prompts: [
-  // generic prompts
-    'dies from laughter',
-    'funny joke',
-    'cringe joke',
-    'sussy noise',
-    'meow~',
-
-    // Neuro-related
-    'praises {x}',
-    'argues with {x}',
-    'gaslights {x}',
-    'loves {x}',
-    'hates {x}',
-    'roasts {x}',
-    'shames {x}',
-    'W RIZZ'
+    ...regularPrompts,
+    ...chatPrompts
   ]
 }
