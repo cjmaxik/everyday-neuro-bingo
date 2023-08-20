ae77b657ba7c802af5fe4d719d81e308df37d96c
CJMAXiK <me@cjmaxik.ru>
2023-08-20 20:18:42 +0300

    Collab with Zentreya
    Closes #90

---
+++ src/prompts/characters/zentreya.js
@@ -0,0 +1,28 @@
+export default {
+  id: 'zentreya',
+  name: 'Zentreya',
+  color: '#d4233f',
+
+  image: '',
+
+  sounds: [],
+
+  prompts: [
+  // generic prompts
+    'dies from laughter',
+    'funny joke',
+    'cringe joke',
+    'sussy noise',
+    'meow~',
+
+    // Neuro-related
+    'praises {x}',
+    'argues with {x}',
+    'gaslights {x}',
+    'loves {x}',
+    'hates {x}',
+    'roasts {x}',
+    'shames {x}',
+    'W RIZZ'
+  ]
+}
