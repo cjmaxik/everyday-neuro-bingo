f69e9c6ed6a943ff502cb9d14d9a087940087356
CJMAXiK <me@cjmaxik.ru>
2023-09-11 18:26:44 +0300

    Initial data structures

---
+++ src/prompts/characters/lia.js
@@ -0,0 +1,24 @@
+export const base = {
+  id: 'lia',
+  name: 'Lia',
+  color: '#a52174',
+
+  image: '',
+
+  sounds: [
+    ''
+  ].map(x => `${x}.mp3`)
+}
+
+// should NOT contain any generic collab prompts
+export const regularPrompts = [
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  ''
+]
